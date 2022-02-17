dc78e0f67b1ecd458e67e0d34410643eb085e4cc
SinlessEvil <89202785+SinlessEvil@users.noreply.github.com>
2022-02-17 22:32:03 +0300

    Update task1.php

--- task1.php
+++ task1.php
@@ -9,33 +9,6 @@
 
 
 1231
-23
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-123123
-
-
-123123
-
-
-123s
-asd
-
-a
-sd
-asd
-ppgppg
-sss
-fsdfs
-
-sdf
-sdfffd
-
-pgppgpgppg
-0
-=======
-123
 >>>>>>> parent of e58b6d2 (Update task1.php)
 =======
 >>>>>>> parent of e68f412 (main)
