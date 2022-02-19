b935610fb15a62fa19e1733fc73e9d94629fc1fb
SinlessEvil <89202785+SinlessEvil@users.noreply.github.com>
2022-02-19 14:30:25 +0300

    Merge branch 'main' of https://github.com/SinlessEvil/JavaScriptPractic

--- task1.php
+++ task1.php
@@ -1,12 +1,22 @@
-123
-123
-123
-123
+<!-- Задание 1 Дан инпут. В него вводится число. По потери фокуса найдите сумму цифр этого числа. -->
+<script>
+		"use strict";
+		let elem = document.querySelector('.main input');
+		let elem1 = document.querySelector('.main');
+		let p = document.createElement('p');
+		
+		elem.addEventListener('blur', function() {
+			console.log(elem);
+			let k = elem.value.split('');
+			console.log(k);
+			let count = 0;
+			for (let s of k){
+				count +=+s;				
+			}
+			p.innerHTML = count;
+			elem1.append(p);
+			console.log(count);
 
-1
-
-231231
-231231
-
-
-1231
+		});
+	</script>
+    <!-- Задание 5: -->
